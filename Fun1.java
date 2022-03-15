93f642359e5754ef51d0b8f9ad5f025c3107d4d5
trunalhajare <101618311+trunalhajare@users.noreply.github.com>
2022-03-15 12:08:53 +0530

    Add files via upload

---
+++ Fun1.java
@@ -0,0 +1,11 @@
+class Fun1{
+static void multiply (int n1, int n2, int n3)
+{
+int result=n1*n2*n3;
+System.out.println("multiply is"+result);
+}
+public static void main(String args[])
+{
+multiply(5,7,9);
+}
+}
